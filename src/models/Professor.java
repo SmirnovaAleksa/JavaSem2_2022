99a5c3254befea2dabc04b4d2dd2d6c1427486d4
Aleksandra Smirnova <smirnovaaleksa39@gmail.com>
2022-03-03 12:11:02 +0200

    Edit: add Set Name functions ir Professors

--- src/models/Professor.java
+++ src/models/Professor.java
@@ -1,5 +1,27 @@
 package models;
 
 public class Professor {
-
+	//1. mainīgie
+	private String name;
+	private String surname;
+	private ProfDegree degree;
+	//2. get funkcijas
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public ProfDegree getDegree() {
+		return degree;
+	}
+	//3. set funkcijas
+	public void setName(String name) {
+		if(name.matches("[A-Z]{1}[a-z]+")) {
+			this.name = name;
+		}
+		else {
+			this.name = "Unknown";
+		}
+	}
 }
