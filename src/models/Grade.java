86c1f22f089f4f530bba5dcac344923c93e58a92
Aleksandra Smirnova <smirnovaaleksa39@gmail.com>
2022-03-10 11:38:31 +0200

    New: Grade class with variables

---
+++ src/models/Grade.java
@@ -0,0 +1,18 @@
+package models;
+
+public class Grade {
+	private int id;
+	private int value;
+	private Student student;
+	private Course course;
+	
+	private static int idCounter = 100000;
+	
+	//2.1 Getters
+	
+	//2.2 Setters
+	
+	//3.Konstruktori
+	
+	//4.toString
+}
