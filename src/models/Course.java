e76ac896952c3e9ac04499251472a5729f9a4fd6
Aleksandra Smirnova <smirnovaaleksa39@gmail.com>
2022-03-10 11:10:21 +0200

    Add: Setters and Getters to Course class

--- src/models/Course.java
+++ src/models/Course.java
@@ -4,9 +4,58 @@ public class Course {
 	//1. Msinīgie
 	
 	private int id;
-	private String title;
-	private byte creditPoints;
-	private Professor professor;
+	private String title; 
+	private byte creditPoints; //Max 20
+	private Professor professor; // Vai not null
+	
+	private static int idCounter = 1000;
+	
+	//2.1 Getters
+	public int getId() {
+		return id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public byte getCreditPoints() {
+		return creditPoints;
+	}
+	public Professor getProfessor() {
+		return professor;
+	}
+	
+	//2.2 Setters
+	public void setTitle(String title) {
+		if(title!= null && title.matches("[A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[\\s a-zēūīāšģķļņčžA-ZĀĒŪĪĻĶĢŠŽČŅ]+\\s?+[0-9]?")) {
+			this.title = title;
+		}
+		else {
+			this.title = "Unknown";
+		}
+	}
+	public void setCreditPoints(byte creditPoints) {
+		if(creditPoints > 0 && creditPoints <= 20) {
+			this.creditPoints = creditPoints;
+		}
+		else {
+			this.creditPoints = 0;
+		}
+	}
+	public void setProfessor(Professor professor) {
+		if(professor!=null) {
+			this.professor = professor;
+		}
+		else {
+			this.professor = new Professor();
+		}
+	}
+	public void setId(int id) {
+		this.id = idCounter;
+		idCounter++;
+	}
+	
+
+	
 	
 	
 }
