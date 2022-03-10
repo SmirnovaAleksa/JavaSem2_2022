decca8d36f4b31328b1fcb838542f7986184306c
Aleksandra Smirnova <smirnovaaleksa39@gmail.com>
2022-03-10 11:32:12 +0200

    Add: Setters and Getters to Student class

--- src/models/Student.java
+++ src/models/Student.java
@@ -20,8 +20,32 @@ public class Student {
 	public String getSurname() {
 		return surname;
 	}
-	
+
 	//2.2 Setters
+	public void setId(int id) {
+		this.id = idCounter;
+		idCounter++;
+	}
+
+	public void setName(String name) {
+		if(name.matches("[A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[a-zēūīāšģķļņčž]+")) {
+			this.name = name;
+		}
+		else {
+			this.name = "Unknown";
+		}
+	}
+
+	public void setSurname(String surname) {
+		if(name.matches("[A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[a-zēūīāšģķļņčž]+")) {
+			this.surname = surname;
+		}
+		else {
+			this.surname = "Unknown";
+		}
+	}
+	
+	//3.Konstructori
 	
 	
 }
